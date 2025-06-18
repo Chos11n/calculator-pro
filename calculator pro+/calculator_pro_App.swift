dfa1107f5cee0ff942cb9159267de622842ba724
Jace Li <1914666787@qq.com>
2025-06-18 10:55:52 +0800

    Initial Commit

---
+++ calculator pro+/calculator_pro_App.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_pro_App.swift
+//  calculator pro+
+//
+//  Created by Li on 2025/6/18.
+//
+
+import SwiftUI
+
+@main
+struct calculator_pro_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calculator pro+Tests/calculator_pro_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  calculator_pro_Tests.swift
+//  calculator pro+Tests
+//
+//  Created by Li on 2025/6/18.
+//
+
+import Testing
+
+struct calculator_pro_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
